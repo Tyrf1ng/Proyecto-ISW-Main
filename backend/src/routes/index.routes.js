a190578deefcf9b3f44a8f1d61cf7ef16ffc063d
Nicoflenn <nicolas.ibieta2101@alumnos.ubiobio.cl>
2024-10-30 19:53:01 -0300

    hola

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,12 +3,8 @@ import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import cursoRoutes from "./curso.routes.js";
 import asistenciaRoutes from "./asistencia.routes.js";
-<<<<<<< HEAD
-import labRoutes from "./lab.routes.js";
-=======
 import anotacionRoutes from "./anotaciones.routes.js";
 import notasRoutes from "./notas.routes.js";
->>>>>>> main
 
 const router = Router();
 
@@ -16,12 +12,8 @@ router
     .use("/auth", authRoutes)
     .use("/cursos", cursoRoutes)
     .use("/asistencias", asistenciaRoutes)
-<<<<<<< HEAD
-    .use("/labs", labRoutes);
-=======
     .use("/anotaciones", anotacionRoutes)
     .use("/notas", notasRoutes)
     .use("/asistencias", asistenciaRoutes);
 
->>>>>>> main
 export default router;
