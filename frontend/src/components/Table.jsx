8635cc026f92c0f1d5c7537e513515d5612c782a
BenjahYKPZz <benjamin.ortiz2101@alumnos.ubiobio.cl>
2024-12-05 23:34:38 -0300

    Cambios locuelos

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -2,7 +2,7 @@ import { IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-const TableComponent = ({ anotaciones, handleOpen, handleDelete }) => {
+const TableAnotacionComponent = ({ anotaciones, handleOpen, handleDelete, role }) => {
   return (
     <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
@@ -18,15 +18,22 @@ const TableComponent = ({ anotaciones, handleOpen, handleDelete }) => {
                 <th className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                   Tipo
                 </th>
-                <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                  RUT del Alumno
-                </th>
+                
                 <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                   Fecha de Creación
                 </th>
-                <th className="relative py-3.5 px-4">
-                  <span className="sr-only">Acciones</span>
-                </th>
+                {role === 'Docente' && (
+                  <>
+                  <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    RUT del Alumno
+                  </th>
+                  <th className="relative py-3.5 px-4">
+                    <span className="sr-only">Acciones</span>
+                  </th>
+                  </>
+                
+                )}
+                
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
@@ -51,22 +58,28 @@ const TableComponent = ({ anotaciones, handleOpen, handleDelete }) => {
                         {anotacion.tipo}
                       </div>
                     </td>
+                    {role === 'Docente' && (
+                    <>
                     <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="text-gray-800 dark:text-white">{anotacion.rut_alumno}</div>
-                    </td>
+                    <div className="text-gray-800 dark:text-white">{anotacion.rut}</div>
+                  </td>  
+                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                    <div className="flex space-x-2">
+                      <IconButton color="primary" onClick={() => handleOpen(anotacion)}>
+                        <EditIcon />
+                      </IconButton>
+                      <IconButton color="primary" onClick={() => handleDelete(anotacion.id_anotacion)}>
+                        <DeleteIcon className="text-red-500" />
+                      </IconButton>
+                    </div>
+                  </td> 
+                  </>
+                  )}
+                    
                     <td className="px-4 py-4 text-sm whitespace-nowrap">
                       <div className="text-gray-800 dark:text-white">{new Date(anotacion.createdAt).toLocaleDateString()}</div>
                     </td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="flex space-x-2">
-                        <IconButton color="primary" onClick={() => handleOpen(anotacion)}>
-                          <EditIcon />
-                        </IconButton>
-                        <IconButton color="primary" onClick={() => handleDelete(anotacion.id_anotacion)}>
-                          <DeleteIcon className="text-red-500" />
-                        </IconButton>
-                      </div>
-                    </td>
+                    
                   </tr>
                 ))
               ) : (
@@ -82,4 +95,4 @@ const TableComponent = ({ anotaciones, handleOpen, handleDelete }) => {
   );
 };
 
-export default TableComponent;
+export default TableAnotacionComponent;
