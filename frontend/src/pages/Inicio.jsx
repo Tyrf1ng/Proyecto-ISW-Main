1693ba0f1a64cd621f6ff297ce12ef5d132a3a89
JoacoPL1 <joaquin.perez2101@alumnos.ubiobio.cl>
2024-12-17 12:40:01 -0300

    aparecen los nombre de los profesores para cada asignatura (alumnos)

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -83,7 +83,7 @@ function Inicio() {
                   <span>
                     Bienvenido a tu página de la asignatura{' '}
                     <span className="text-[#3B82F6]">
-                      {asignatura && asignatura.nombre ? asignatura.nombre : 'Cargando asignatura...'}
+                      {asignatura && asignatura.nombre? asignatura.nombre : 'Cargando asignatura...'}
                     </span>
                   </span>
                 )}
